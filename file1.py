dac20df894a5e17719a12938c413ba23c0b5bff4
ifilearn <ifilearn@gmail.com>
2021-09-14 22:34:45 +0800

    add again to master

--- file1.py
+++ file1.py
@@ -1,3 +1,4 @@
+print('add to master again')
 print('test')
 print('test in branch new-feature')
 print('test in branch extended')
--- file2.py
+++ file2.py
@@ -1,3 +1,4 @@
+print('add to master agaain')
 print('hello world!')
 print('hellow world in branch new-feature')
 print('hellow world in branch extended')
