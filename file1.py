efb171363c1d16f688718740c4542030447165b5
ifilearn <ifilearn@gmail.com>
2021-09-14 22:14:45 +0800

    create files in branch extended

--- file1.py
+++ file1.py
@@ -1,2 +1,3 @@
 print('test')
 print('test in branch new-feature')
+print('test in branch extended')
--- file2.py
+++ file2.py
@@ -1,2 +1,3 @@
 print('hello world!')
 print('hellow world in branch new-feature')
+print('hellow world in branch extended')
